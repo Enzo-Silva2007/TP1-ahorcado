e18d46455ea3ff7be0ab7fd7efc3ac04b6e372ef
can <manas.candela@gmail.com>
2025-10-25 22:32:40 -0300

    archivo de constantes.py xd

---
+++ constantes.py
@@ -0,0 +1,19 @@
+# estados del juego
+ESTADO_MENU = "menu"
+ESTADO_JUGADOR = "jugador"
+ESTADO_OPCIONES = "opciones"
+ESTADO_JUEGO = "juego"
+ESTADO_SALIR = "salir"
+ESTADO_SEGUIR = "seguir"
+
+# teclas necesarias para el juego
+K_UP = "w"
+K_DOWN = "s"
+K_ENTER = "enter"
+K_ESC = "esc"
+K_SPACE = "space"
+K_PLAY = "p"
+K_NEW_USER = "r"
+K_LOGIN = "i"
+K_YES = "s"
+K_NO = "n"
