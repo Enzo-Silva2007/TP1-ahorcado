acee8aa80480e72e1ff0098eb90de0ccef09e399
Gian Franco Miguel Caputo <gianfrancocap12@gmail.com>
2025-11-02 21:32:57 -0300

    Controladores arreglados. Todas las pantallas usan el modulo keyboard para el ingreso de datos

--- validacion.py
+++ validacion.py
@@ -1,22 +1,5 @@
 import re
-
-# funciones del modelo
-
-def tamanio_palabra(dificultad, nueva_palabra):
-    longitud = len(nueva_palabra)
-
-    if dificultad == "facil":
-        if longitud > 6:
-            return False
-    elif dificultad == "medio":
-        if longitud < 6 or longitud > 8:
-            return False
-    elif dificultad == "dificil":
-        if longitud < 8 or longitud > 13:
-            return False
-
-    return True
-
+import keyboard
 
 # funciones del controlador
 
@@ -29,14 +12,12 @@ def nombre_valido(nombre):
     # solo letras a-z sin acentos ni ñ
     return re.fullmatch(r"[a-z]+", n) is not None
 
-
 def letra_valida(letra):
     if not isinstance(letra, str):
         return False
     l = letra.strip().lower()
     # tiene que ser una sola letra a-z
     return re.fullmatch(r"[a-z]", l) is not None
-
 
 def palabra_valida(palabra_secreta, palabra):
     if not isinstance(palabra, str):
